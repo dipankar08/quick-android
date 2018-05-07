29ddf0cfd92995988e24fd8f0b07bd756d0f01de
Dipankar Dutta <dip@michellexu-mbp.dhcp.thefacebook.com>
2018-05-07 15:00:31 -0700

    adding bintry sypport

---
+++ publish.sh
@@ -0,0 +1,2 @@
+# Please execute this script if you want to pubslish this repo to bintray
+./gradlew clean build bintrayUpload -PbintrayUser=dipankar08 -PbintrayKey=ca78c759ee7c7ef981a281fa421ee25733ed29d1  -PdryRun=false
